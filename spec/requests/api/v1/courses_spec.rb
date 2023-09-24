73a3f3ba7028f5da249cf41b0a0296a5dbcc2312
unknowntpo <e850506@gmail.com>
2023-09-24 15:33:13 +0800

    (Rspec: requests/API/V1/courses) make test failed

--- spec/requests/api/v1/courses_spec.rb
+++ spec/requests/api/v1/courses_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Courses", type: :request do
+  let(:api_path) { "/api/v1/courses" }
+
   describe "GET /api/v1/courses" do
+    before {get api_path}
     it "works! (now write some real specs)" do
-      get "/api/v1/courses"
+      # get "/api/v1/courses"
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /api/v1/courses" do
+    before { post api_path, params: course_params }
+
+    context "with valid parameters" do
+      let(:course_params) { { name: "Course Name", description: "Course Description" } } # Example valid params
+      it 'new courses should be created' do
+        expect(response).to have_http_status(200)
+      end
+    end
+    context "when we create new courses with wrong params" do
+      let(:course_params) { { name: "", description: "" } } # Example invalid params
+      it 'should return bad request status code' do
+        expect(response).to have_http_status(400)
+      end
     end
   end
 end
