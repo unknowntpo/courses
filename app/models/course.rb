552931027d39fd693c8689b81bcf1d9ed96084ca
unknowntpo <e850506@gmail.com>
2023-10-01 14:25:05 +0800

    (RSpec:Model) refine spec

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,30 @@
+# Represents an academic course. A course can have many chapters and is associated
+# with a specific lecturer.
+#
+# == Schema Information
+#
+# Table name: courses
+#
+# * +id+ :integer          not null, primary key
+# * +name+ :string
+# * +lecturer+ :string
+# * +description+ :text
+# * +created_at+ :datetime not null
+# * +updated_at+ :datetime not null
+#
+# == Relationships
+#
+# has_many :chapters, dependent: :destroy
+#
+# == Validations
+#
+# * Validates the presence of +name+.
+# * Validates the presence of +lecturer+.
+# * +description+ is optional.
+# * +chapters+ is valid in graphql layer.
 class Course < ApplicationRecord
   has_many :chapters, :dependent => :destroy
-  validates_presence_of :name, :lecturer, :description
+  validates_presence_of :name, :lecturer
   accepts_nested_attributes_for :chapters
 
   def self.get_all
