69ee2983393ddf6963d1089a09992c61c9176267
unknowntpo <e850506@gmail.com>
2023-10-01 15:27:54 +0800

    (Rspec:Graphql:CourseDelete) handle course not found while deletion

--- app/graphql/mutations/course_delete.rb
+++ app/graphql/mutations/course_delete.rb
@@ -4,14 +4,27 @@ module Mutations
   class CourseDelete < BaseMutation
     description "Deletes a course by ID"
 
-    field :course, Types::CourseType, null: false
+    field :course, Types::CourseType, null: true
+
+    field :errors, [String], null: true
 
     argument :id, ID, required: true
 
     def resolve(id:)
-      course = ::Course.find(id)
-      { course: nil, errors: course.errors.full_messages } unless course.destroy
-      { course: course }
+      begin
+        course = ::Course.find(id)
+        if course.destroy
+          puts "puts destroy"
+          { course: course }
+        else
+          puts "puts atere"
+          { course: nil, errors: course.errors.full_messages }
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        puts "puts rescue"
+
+        { course: nil, errors: ["Course not found."] }
+      end
     end
   end
 end
--- spec/graphql/mutations/course_delete_spec.rb
+++ spec/graphql/mutations/course_delete_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 module Mutations
   RSpec.describe Mutations::CourseDelete, type: :request do
-    context ".resolve" do
+    context "delete a course" do
       let(:mutation) do
         <<-GQL
       mutation courseDelete($input: CourseDeleteInput!) {
@@ -13,44 +13,76 @@ module Mutations
             lecturer
             description
           }
+          errors
         }
       }
         GQL
       end
-      let(:course) do
-        course = FactoryBot.create(:course, :with_chapters_and_units)
-        course = Course.joins(chapters: :units).find(course.id)
+      context "succeed" do
+        let(:course) do
+          course = FactoryBot.create(:course, :with_chapters_and_units)
+          course = Course.joins(chapters: :units).find(course.id)
+        end
+        let(:variables) do
+          {
+            "input": {
+              "id": course.id,
+            },
+          }
+        end
+
+        it "delete a course" do
+          course_id = course.id
+          chapter_ids = course.chapters.map(&:id)
+          unit_ids = Unit.where(chapter_id: chapter_ids).pluck(:id)
+
+          post "/graphql", params: { query: mutation, variables: variables }
+          body = JSON.parse(response.body)
+          puts "body: #{body.inspect}"
+          data = body["data"]["courseDelete"]["course"]
+
+          expect(data).to include(
+            "id" => course.id.to_s,
+          )
+
+          course_id = course.id
+          chapter_ids = course.chapters.map(&:id)
+          unit_ids = Unit.where(chapter_id: chapter_ids).pluck(:id)
+
+          # Verify they're not found in the database
+          expect(Course.find_by(id: course_id)).to be_nil
+          expect(Chapter.find_by(id: chapter_ids)).to be_nil
+          expect(Unit.find_by(id: unit_ids)).to be_nil
+        end
       end
-      let(:variables) do
-        {
-          "input": {
-            "id": course.id,
-          },
-        }
-      end
+      context "failed" do
+        let(:course) do
+          course = FactoryBot.create(:course, :with_chapters_and_units)
+          course = Course.joins(chapters: :units).find(course.id)
+        end
+        let(:variables) do
+          {
+            "input": {
+              # invalid id
+              "id": 3746593,
+            },
+          }
+        end
 
-      it "delete a course" do
-        course_id = course.id
-        chapter_ids = course.chapters.map(&:id)
-        unit_ids = Unit.where(chapter_id: chapter_ids).pluck(:id)
+        it "delete a course" do
+          course_id = course.id
+          chapter_ids = course.chapters.map(&:id)
+          unit_ids = Unit.where(chapter_id: chapter_ids).pluck(:id)
 
-        post "/graphql", params: { query: mutation, variables: variables }
-        body = JSON.parse(response.body)
-        puts "body: #{body.inspect}"
-        data = body["data"]["courseDelete"]["course"]
+          post "/graphql", params: { query: mutation, variables: variables }
+          body = JSON.parse(response.body)
+          puts "body: #{body.inspect}"
+          data = body["data"]["courseDelete"]
 
-        expect(data).to include(
-                          "id" => course.id.to_s,
-                        )
-
-        course_id = course.id
-        chapter_ids = course.chapters.map(&:id)
-        unit_ids = Unit.where(chapter_id: chapter_ids).pluck(:id)
-
-        # Verify they're not found in the database
-        expect(Course.find_by(id: course_id)).to be_nil
-        expect(Chapter.find_by(id: chapter_ids)).to be_nil
-        expect(Unit.find_by(id: unit_ids)).to be_nil
+          expect(data).to include(
+            "course" => nil, "errors" => ["Course not found."],
+          )
+        end
       end
     end
   end
