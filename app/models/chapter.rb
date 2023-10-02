e0ac4df954a555ccf0603700ceae903167dd9ea9
unknowntpo <e850506@gmail.com>
2023-10-02 14:09:43 +0800

    remove uniqueness check
    because if we reorder the chapters and units, we need to udpate the
    position one by one, then the uniqueness will bite us.

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -3,7 +3,4 @@ class Chapter < ApplicationRecord
   has_many :units, :dependent => :destroy
   validates_presence_of :name
   accepts_nested_attributes_for :units
-
-  # https://guides.rubyonrails.org/active_record_validations.html#uniqueness
-  validates :position, uniqueness: { scope: :course_id }
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,5 +1,4 @@
 class Unit < ApplicationRecord
   belongs_to :chapter
   validates_presence_of :name, :description, :content
-  validates :position, uniqueness: { scope: :chapter_id }
 end
