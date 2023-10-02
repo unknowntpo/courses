aec992fc84ee7c580da8012e4dd8cab15351597f
unknowntpo <e850506@gmail.com>
2023-10-02 20:36:01 +0800

    (Rspec:Model:ChapterSpec): fix failed test

--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -1,13 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Chapter, type: :model do
-  describe 'validation' do
-    it 'name should be present' do
+  describe "validation" do
+    it "name should be present" do
       chapter = Chapter.new()
-      puts "chapter: #{chapter.inspect}"
       expect(chapter).not_to be_valid
       expect(chapter.errors[:name]).to include("can't be blank")
-      expect(chapter.errors[:units]).to include("can't be blank")
     end
   end
 end
