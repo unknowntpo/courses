6ab3e12ef48b632258187cdce5ffb67f720b59a1
unknowntpo <e850506@gmail.com>
2023-10-02 20:37:50 +0800

    (Rspec:Model:UnitSpec): fix failed test

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Unit, type: :model do
-  describe 'validation' do
-    it 'name should be present' do
+  describe "validation" do
+    it "name should be present" do
       unit = Unit.new()
       puts "unit: #{unit.inspect}"
       expect(unit).not_to be_valid
       expect(unit.errors[:name]).to include("can't be blank")
-      expect(unit.errors[:description]).to include("can't be blank")
+      expect(unit.errors.has_key? :description).not_to be_truthy
       expect(unit.errors[:content]).to include("can't be blank")
     end
   end
