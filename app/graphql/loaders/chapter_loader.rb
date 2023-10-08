bc49a631c0495e85c728f28a86339f974009a338
unknowntpo <e850506@gmail.com>
2023-10-08 19:23:14 +0800

    (GraphQL::Loaders::ChapterLoader) add loader impl

--- app/graphql/loaders/chapter_loader.rb
+++ app/graphql/loaders/chapter_loader.rb
@@ -1,34 +1,11 @@
-# module Loaders
-#   class ChapterLoader < GraphQL::Batch::Loader
-#     def initialize(model)
-#       @model = model
-#     end
+module Loaders
+  class ChapterLoader < GraphQL::Batch::Loader
+    def initialize(model)
+      @model = model
+    end
 
-#     # def perform(course_ids)
-#     #   Chapter.where(course_id: course_ids).group_by(&:course_id).each do |course_id, chapters|
-#     #     fulfill(course_id, chapters)
-#     #   end
-
-#     #   course_ids.each { |id| fulfill(id, []) unless fulfilled?(id) }
-#     # end
-
-#     def perform(course_ids)
-#       Chapter.where(course_id: course_ids).group_by(&:course_id).each { |course_id, chapters| fulfill(course_id, chapters) }
-#       course_ids.each { |course_id| fulfill(course_id, []) unless fulfilled?(course_id) }
-#     end
-#   end
-
-#   class UnitLoader < Dataloader::BatchLoader
-#     # def perform(chapter_ids)
-#     #   Unit.where(chapter_id: chapter_ids).group_by(&:chapter_id).each do |chapter_id, units|
-#     #     fulfill(chapter_id, units)
-#     #   end
-
-#     #   chapter_ids.each { |id| fulfill(id, []) unless fulfilled?(id) }
-#     # end
-#     def perform(chapter_ids)
-#       Unit.where(chapter_id: chapter_ids).group_by(&:chapter_id).each { |chapter_id, units| fulfill(chapter_id, units) }
-#       chapter_ids.each { |chapter_id| fulfill(chapter_id, []) unless fulfilled?(chapter_id) }
-#     end
-#   end
-# end
+    def perform(ids)
+      @model.where(id: ids).each { |chapter| fulfill(chapter.id, chapter) }
+    end
+  end
+end
--- spec/graphql/loaders/course_loader_spec.rb
+++ spec/graphql/loaders/course_loader_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Loaders::CourseLoader do
   let!(:course1) { FactoryBot.create(:course, :with_chapters_and_units) }
   let!(:course2) { FactoryBot.create(:course, :with_chapters_and_units) }
 
-  describe "when we load file from loader" do
+describe "when we load course from loader" do
     it "should load courses" do
       result = GraphQL::Batch.batch do
         Loaders::CourseLoader.for(Course).load(course1.id)
